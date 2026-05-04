a1fcfd0e8073defd79f8ed84600b914f222428e8
janVoice <janvoice04@gmail.com>
2026-05-04 10:39:22 +0530

    admin

--- authSlice.js
+++ authSlice.js
@@ -1,30 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const savedUser = JSON.parse(localStorage.getItem("user"));
-const savedToken = localStorage.getItem("token");
-
 const initialState = {
   user: JSON.parse(localStorage.getItem("user")) || null,
   token: localStorage.getItem("token") || null,
   isLoggedIn: !!localStorage.getItem("token"),
 };
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
+
     setLogin: (state, action) => {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
-    },
-    logout: (state) => {
-  state.user = null;
-  state.token = null;
-  state.isLoggedIn = false;
 
-  localStorage.removeItem("user");
-  localStorage.removeItem("token");
-}
+      // ✅ store in localStorage (centralized)
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
+      localStorage.setItem("token", action.payload.token);
+    },
+
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      state.isLoggedIn = false;
+
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+    },
   },
 });
 
